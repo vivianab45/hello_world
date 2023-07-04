71f45935566bde035fe12562091ede431d30cbc1
vivianab45 <balleza45@gmail.com>
2023-07-03 21:45:09 -0500

    initial commit

---
+++ hello_world.py
@@ -0,0 +1,16 @@
+# 1. TASK: print "Hello World"
+print("Hello World")
+# 2. print "Hello Noelle!" with the name in a variable
+name = "Viviana"
+print("Hello", name,"!")
+print("Hello " + name + "!")
+
+name=42
+print("Hello", name, "!")
+print("Hello " + str(name) + "!")
+
+fave_food1 = "eggs"
+fave_food2= "beans"
+
+print ( "I love to eat {} and {}." .format(fave_food1, fave_food2))
+print ( f"I love to eat {fave_food1} and {fave_food2}. ")
